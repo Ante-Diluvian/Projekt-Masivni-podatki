59df3c1b188e6382bac527832f0fa510be359e0c
WoLfe7z <jaka.kocar@gmail.com>
2025-05-19 18:34:17 +0200

    Added bottom tabs for navigation
    Added bottom tabs for better navigation between screens

--- NPO/App.js
+++ NPO/App.js
@@ -2,22 +2,34 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+
+//Navigation
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 //Screens
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 //import MainScreen from './screens/MainScreen';
 
+const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
+
+function LoginStack() {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Register" component={RegisterScreen} />
+    </Stack.Navigator>
+  );
+}
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Register" component={RegisterScreen} />
-      </Stack.Navigator>
+      <Tab.Navigator screenOptions={{ headerShown: false }}>
+        <Tab.Screen name="Login" component={LoginStack} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
