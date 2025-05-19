38efab3107a3fb59b8bb1991a77db2956befa9e8
tilrn1 <tilen.bratkovic@student.um.si>
2025-05-19 18:07:20 +0200

    Added Gaussian filter

--- ORV/augmentacija.py
+++ ORV/augmentacija.py
@@ -1,14 +1,52 @@
 import cv2 as cv
 import numpy as np
+import math
+
+def procesiraj_sliko(pot):
+    slika = cv.imread(pot)
+    sivinska_slika = cv.cvtColor(slika, cv.COLOR_BGR2GRAY)
+    return sivinska_slika
+    pass
+
+def konvolucija(slika, jedro):
+    visina, sirina = slika.shape
+    j_visina, j_sirina = jedro.shape
+
+    pad_v = j_visina // 2
+    pad_s = j_sirina // 2
+      
+    
+    razsirjena_slika = np.pad(slika, ((pad_v, pad_v), (pad_s, pad_s)))
+    filtrirano = np.zeros_like(slika, dtype=np.float32)
+    for i in range(visina):
+        for j in range(sirina):
+            filtrirano[i, j] = np.sum(razsirjena_slika[i:i+j_visina, j:j+j_sirina] * jedro)
+    
+    return filtrirano
+    pass
+
+def filtriraj_z_gaussovim_jedrom(slika,sigma):
+    velikost_jedra = (int)(2 * sigma) * 2 + 1  
+    k = (velikost_jedra / 2) - (1/2)
+  
+    jedro = np.zeros((velikost_jedra, velikost_jedra), dtype=np.float32)
+    for i in range(velikost_jedra):
+        for j in range(velikost_jedra):
+            jedro[i,j] = (1 / (2 * math.pi * math.pow(sigma, 2)) * math.exp(-(math.pow((i - k - 1), 2) + math.pow((j - k - 1), 2)) / (2 * math.pow(sigma, 2))))
+  
+    jedro /= np.sum(jedro)
+    return konvolucija(slika,jedro)
+    pass
 
 if __name__ == "__main__":
-    slika = cv.imread('test/clovek.jpg')
+    slika = procesiraj_sliko("test/clovek.jpg")
     slika = cv.resize(slika,(500,700))
+    slika = filtriraj_z_gaussovim_jedrom(slika,5)
     if slika is None:
         print("Napaka: Slika ni bila naloÅ¾ena. Preveri pot do slike.")
     else:
         while True:  
-            cv.imshow('Slika', slika)
+            cv.imshow('Slika', slika.astype(np.uint8))
             if cv.waitKey(1) & 0xFF == ord('q'):
                 break
 
