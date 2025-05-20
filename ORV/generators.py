c4ae049fa400c83843f22c26d2059ebedd15dbc5
WoLfe7z <jaka.kocar@gmail.com>
2025-05-20 18:51:38 +0200

    Implemented __init__ and __len__
    Implemented the __init__ and __len__ logic in generators.py

--- ORV/generators.py
+++ ORV/generators.py
@@ -1,10 +1,26 @@
+import numpy as np
+import cv2 as cv
+import tensorflow as tf
+import random
+
+from tensorflow.keras.utils import Sequence
+
+from augmentacija import process_img, filter_with_gausso_core, linearize_img, rotate_img
+
 class ImageDataGenerator(tf.keras.utils.Sequence):
     def __init__(self, image_paths, labels, batch_size=32, image_size=(64, 64), shuffle=True, augment=False):
         #Initialize the data generator
-        pass
+        self.image_paths = image_paths
+        self.labels = labels
+        self.batch_size = batch_size
+        self.image_size = image_size
+        self.shuffle = shuffle
+        self.augment = augment
+        self.on_epoch_end()
 
     def __len__(self):
         #Return the number of batches per epoch
+        return int(np.ceil(len(self.image_paths) / self.batch_size))
         pass
 
     def __getitem__(self, idx):
