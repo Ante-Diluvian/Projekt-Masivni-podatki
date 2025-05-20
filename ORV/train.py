e8fa054f1fd4508015c85148fd0bc571f95d8080
WoLfe7z <jaka.kocar@gmail.com>
2025-05-20 19:32:41 +0200

    Updated train.py
    Included all necesary files, added comments for future development

---
+++ ORV/train.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+
+from utils import load_images_and_labels
+from generators import ImageDataGenerator
+from models import build_model
+
+def train():
+    #1.Load images and labels
+    #2.Split data into training and validation sets
+    #3.Create data generators
+    #4.Build the model
+    #5.Compile the model
+    #6.Train the model
+    #7.Save the model
+    pass
+
+if __name__ == "__main__":
+    train()
