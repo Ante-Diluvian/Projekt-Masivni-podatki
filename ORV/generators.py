d87f84e09c0d754ae0f2d5b9134933670bb58343
WoLfe7z <jaka.kocar@gmail.com>
2025-05-20 18:38:11 +0200

    Simple architecture for image data generator
    Added a simple architecture for the image data generator. Not yet implemented

--- ORV/generators.py
+++ ORV/generators.py
@@ -1,1 +1,24 @@
-'''augment_image, load_image, ...'''
+class ImageDataGenerator(tf.keras.utils.Sequence):
+    def __init__(self, image_paths, labels, batch_size=32, image_size=(64, 64), shuffle=True, augment=False):
+        #Initialize the data generator
+        pass
+
+    def __len__(self):
+        #Return the number of batches per epoch
+        pass
+
+    def __getitem__(self, idx):
+        #Generate one batch of data
+        pass
+    
+    def on_epoch_end(self):
+        #Updates the indexes after each epoch
+        pass
+
+    def load_image(self, path):
+        #Load and preprocess the image
+        pass
+    
+    def augment_image(self, img):
+        #Apply data augmentation to the image
+        pass
