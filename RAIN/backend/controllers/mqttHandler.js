ee391aa10e52088f99eaed721a06a70496994722
Ante-Diluvian <val.smodis@student.um.si>
2025-05-22 19:15:41 +0200

    updated backend for user count

--- RAIN/backend/controllers/mqttHandler.js
+++ RAIN/backend/controllers/mqttHandler.js
@@ -3,15 +3,31 @@ const userController = require('./userController');
 const workoutController = require('./workoutController');
 const Gps = require('../models/gpsModel.js')
 const Accelerometer = require('../models/accelerometerModel')
+const path = require('path');
+const fs = require('fs');
+
+const logFile = parh.join(__dirname, '../logs/mqtt_status.log');
+const activeUsers = new Set();
 
 const client = mqtt.connect('ws://194.163.176.154:9001', {
   username: 'app_guest',
   password: 'fentanyl'
 });
 
+function logEvent(type, userId){
+  const timestamp = new Date().toISOString();
+  const line = `[${timestamp}] ${type.toUpperCase()} - ${userId}\n`;
+  fs.appendFile(logFile, line, (err) => {
+    if (err) {
+      console.error('Error writing to log file:', err);
+    }
+  });
+  console.log(line.trim());
+}
+
 client.on('connect', () => {
   console.log('✅ Connected to MQTT broker');
-
+  client.subscribe(['status/online', 'status/offline']);
   client.subscribe("app/workout", (err, granted) => {
     if (err) {
       console.error("Subscribe error:", err);
@@ -51,8 +67,20 @@ client.on('message', (topic, messageBuffer) => {
     } catch (err) {
       console.error('Failed to parse MQTT message:', err);
     }
+  } else if (topic === 'status/online') {
+    const userId = messageBuffer.toString();
+    activeUsers.add(userId);
+    logEvent('online', userId);
+    console.log(`Active users: ${activeUsers.size}`);
+  }
+   else if (topic === 'status/offline') {
+    const userId = messageBuffer.toString();
+    activeUsers.delete(userId);
+    logEvent('offline', userId);
+    console.log(`Active users: ${activeUsers.size}`);
   }
 });
+
   client.on('error', (err) => {
     console.error('MQTT error:', err);
   });
