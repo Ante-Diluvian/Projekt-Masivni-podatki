4551a8ed2894c7f031b754ab7458ed716fce1a90
Ante-Diluvian <val.smodis@student.um.si>
2025-05-23 21:37:29 +0200

    added preprocessing and cosine similarity functions

--- ORV/server/app.py
+++ ORV/server/app.py
@@ -11,10 +11,13 @@ from werkzeug.utils import secure_filename
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 
+# Set up Flask app and upload folder
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads'
 os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 
+# Set up MongoDB connection
+# Ensure you have the environment variables set for MongoDB connection
 user = os.getenv("MONGO_USER")
 password = os.getenv("MONGO_PASS")
 host = os.getenv("MONGO_HOST")
@@ -23,7 +26,16 @@ client = MongoClient(mongo_uri)
 db = client["face_auth"]
 users_collection = db["users"]
 
-mongo_uri = f"mongodb+srv://{user}:{password}@{host}/?retryWrites=true&w=majority&appName=Massive"
-client = MongoClient(mongo_uri)
-
+# Load the pre-trained model
 embedding_model = tf.keras.models.load_model("model/final_model.keras")
+
+# Preprocess the image
+def preprocess_image(img_path, target_size=(112, 112)):
+    img = image.load_img(img_path, target_size=target_size)
+    img_array = image.img_to_array(img) / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
+    return img_array
+
+# Cosine similarity
+def cosine_similarity(a, b):
+    return np.dot(a, b.T) / (np.linalg.norm(a) * np.linalg.norm(b))
