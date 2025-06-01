07146dbfe802093ae666322645180fa259a1e923
tilrn1 <tilen.bratkovic@student.um.si>
2025-06-01 15:38:57 +0200

    Updated recipes and recipe

--- RAIN/frontend/src/components/Recipes.js
+++ RAIN/frontend/src/components/Recipes.js
@@ -16,26 +16,33 @@ function Recipes() {
     }, []);
 
   return (
-    <div className="vh-200 pt-5 mt-5 grid grid-cols-1 md:grid-cols-2 gap-6 max-w-4xl mx-auto p-4">
-    {recipes.map((recipe) => (
-      <div key={recipe._id} className="bg-white rounded-2xl shadow-md p-4 hover:shadow-lg transition-shadow" >
-        <h2 className="text-2xl font-bold mb-2">{recipe.name}</h2>
-
-        {recipe.nutrition && (
-          <div className="text-sm text-gray-600 mb-2">
-            <p>Calories: {recipe.nutrition.calories}</p>
-            <p>Fat: {recipe.nutrition.fat}</p>
-            <p>Carbs: {recipe.nutrition.carbs}</p>
-            <p>Protein: {recipe.nutrition.protein}</p>
-          </div>
-        )}
-
-        <Link to={`/recipes/${recipe._id}`} className="text-blue-500 hover:underline">
-          View Recipe
-        </Link>        
+    <section className="mt-5 recipes-container">
+      <div className="recipes-header">
+        <h2 className="recipes-title">EXPLORE OUR</h2>
+        <h1 className="recipes-subtitle">RECIPES</h1>
       </div>
-    ))}
-  </div>
+
+      <div className="recipes-grid">
+        {recipes.map((recipe) => (
+          <div key={recipe._id} className="recipe-card">
+            <h3 className="recipe-name">{recipe.name}</h3>
+
+            {recipe.nutrition && (
+              <div className="recipe-nutrition">
+                <p>Calories: <span>{recipe.nutrition.calories}</span></p>
+                <p>Fat: <span>{recipe.nutrition.fat}g</span></p>
+                <p>Carbs: <span>{recipe.nutrition.carbs}g</span></p>
+                <p>Protein: <span>{recipe.nutrition.protein}g</span></p>
+              </div>
+            )}
+
+            <Link to={`/recipes/${recipe._id}`} className="recipe-button-text recipe-link">
+              View Recipe
+            </Link>        
+          </div>
+        ))}
+      </div>
+    </section>
   );
 }
 
