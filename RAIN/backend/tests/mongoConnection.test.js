a7f8536b224657cb22fca737cbc9e256f4f53cba
WoLfe7z <jaka.kocar@gmail.com>
2025-06-09 17:35:59 +0200

    Added waiting
    Added waiting for test to end

--- RAIN/backend/tests/mongoConnection.test.js
+++ RAIN/backend/tests/mongoConnection.test.js
@@ -1,9 +1,12 @@
 require('dotenv').config();
+jest.setTimeout(30000);
+
 const mongoose = require('mongoose');
 
 describe('MongoDB connection (app.js)', () => {
   beforeAll(async () => {
     if (mongoose.connection.readyState === 0) {
+      console.log('Connecting to:', process.env.MONGO_URI);
       await mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
@@ -13,6 +16,7 @@ describe('MongoDB connection (app.js)', () => {
 
   afterAll(async () => {
     await mongoose.disconnect();
+    await new Promise(r => setTimeout(r, 500));
   });
 
   test('mongoose should be connected', () => {
