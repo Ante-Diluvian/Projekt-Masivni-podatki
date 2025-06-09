7f0fc83c0968eacc8819170ae0b5d8249b296223
WoLfe7z <jaka.kocar@gmail.com>
2025-06-09 18:05:24 +0200

    Fix to liveServer
    Fix to liveServer test file

--- RAIN/backend/tests/liveServer.test.js
+++ RAIN/backend/tests/liveServer.test.js
@@ -1,9 +1,11 @@
 const axios = require('axios');
 
-describe('Live server test', () => {
-  test('GET / should return status 200', async () => {
-    const response = await axios.get('http://localhost:3001/');
-    expect(response.status).toBe(200);
-    expect(response.data).toContain('RAIN');
+jest.setTimeout(10000);
+
+describe('Live server test (existing app.js)', () => {
+  test('GET / should return 200', async () => {
+    const res = await axios.get('http://localhost:3001/');
+    expect(res.status).toBe(200);
+    expect(res.data).toContain('RAIN');
   });
 });
