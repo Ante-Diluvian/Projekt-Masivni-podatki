0cc407b8ec2af4eb3e9b11de25096eaa462f58bd
WoLfe7z <jaka.kocar@gmail.com>
2025-06-09 17:15:58 +0200

    Fixed MongoConnection test
    Fixed mongo connection test completing before connection setup

--- RAIN/backend/tests/mongoConnection.test.js
+++ RAIN/backend/tests/mongoConnection.test.js
@@ -1,6 +1,19 @@
 const mongoose = require('mongoose');
 
 describe('MongoDB connection (app.js)', () => {
+  beforeAll(async () => {
+    if (mongoose.connection.readyState === 0) {
+      await mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+    }
+  });
+
+  afterAll(async () => {
+    await mongoose.disconnect();
+  });
+
   test('mongoose should be connected', () => {
     expect([1, 2]).toContain(mongoose.connection.readyState);
   });
